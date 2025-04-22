53d6ae94a9c1511ffea99f362c141412a27c9beb
Svyatoslav Vlasov <s.vlasov@axitech.ru>
2025-04-22 09:48:51 +0300

    ObjectsUtils requireNonNull 1.1

--- src/main/java/ru/byzatic/commons/ObjectsUtils.java
+++ src/main/java/ru/byzatic/commons/ObjectsUtils.java
@@ -1,6 +1,6 @@
 package ru.byzatic.commons;
 public class ObjectsUtils {
-    public static <T> void requireNonNull(T obj, Throwable exception) throws Throwable {
+    public static <T, E extends Throwable> void requireNonNull(T obj, E exception) throws E {
         if (obj == null) {
             throw exception;
         }
